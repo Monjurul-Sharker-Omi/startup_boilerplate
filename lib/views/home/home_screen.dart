d0977b94c17732077b7ba6510c960990aa958933
sharkeromi <genie.sharkeomi@gmail.com>
2024-06-07 18:09:20 +0600

    feat: added env files

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:startup_boilerplate/controllers/home/home_controller.dart';
 import 'package:startup_boilerplate/utils/constants/imports.dart';
 import 'package:startup_boilerplate/views/widgets/common/buttons/custom_button.dart';
-import 'package:startup_boilerplate/views/widgets/common/textfield/custom_textfield.dart';
 import 'package:startup_boilerplate/views/widgets/common/utils/custom_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -32,16 +31,6 @@ class HomeScreen extends StatelessWidget {
           body: Padding(
             padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
             child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              // CustomModifiedTextField(
-              //   hint: "Enter github token",
-              //   controller: homeController.tokenTextEditingController,
-              // ),
-              // kH16sizedBox,
-              // CustomElevatedButton(
-              //     label: "Save token",
-              //     onPressed: () {
-              //       homeController.saveToken(homeController.tokenTextEditingController.text.trim());
-              //     }),
               kH16sizedBox,
               CustomElevatedButton(
                 label: "Get Flutter Repositories",
